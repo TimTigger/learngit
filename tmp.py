7083f9da6faeb294edbac54193484ac3b88da56c
Lewis <levviser@gmail.com>
2023-08-24 11:42:21 +0800

    为什么无法上传啊

---
+++ tmp.py
@@ -0,0 +1,46 @@
+# -*- coding:utf-8 -*-
+# coding:utf-8
+import os, sys
+import chardet
+import sys
+
+print(sys.getdefaultencoding())
+reload(sys)
+sys.setdefaultencoding('gbk')
+
+
+def convert(filename, in_enc="GBK", out_enc="UTF8"):
+    try:
+        print("convert " + filename.encode('utf8'))
+        content = open(filename).read()
+        result = chardet.detect(content)  # 通过chardet.detect获取当前文件的编码格式串，返回类型为字典类型
+        coding = result.get('encoding')  # 获取encoding的值[编码格式]
+        if coding != 'utf-8':  # 文件格式如果不是utf-8的时候，才进行转码
+            print(coding + "to utf-8!")
+            new_content = content.decode(in_enc).encode(out_enc)
+            open(filename, 'w').write(new_content)
+            print(" done")
+        else:
+            print(coding)
+    # except IOError,e:
+    except:
+        print(" error")
+
+
+def explore(dirs):
+    for root, dirs, files in os.walk(dirs):
+        for file in files:
+            path = os.path.join(root, file)
+            convert(path)
+
+
+def main():
+    for path in sys.argv[1:]:
+        if os.path.isfile(path):
+            convert(path)
+        elif os.path.isdir(path):
+            explore(path)
+
+
+if __name__ == "__main__":
+    main()
